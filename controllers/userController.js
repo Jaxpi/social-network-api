f36ea83d138ba5f739988fa513b16a28b3473fa1
Jax I <jaxpi85@gmail.com>
2022-11-21 16:44:36 -0500

    worked on routes

---
+++ controllers/userController.js
@@ -0,0 +1,45 @@
+const { Users, Thoughts } = require('../models');
+
+module.exports = {
+  // Get all users
+  getUsers(req, res) {
+    Users.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Get a single user
+  getSingleUser(req, res) {
+    Users.findById(req.params.userId)
+      .select('-__v')
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+//   // create a new user
+  createUser(req, res) {
+    Users.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  updateUser(req, res) {
+    Users.findByIdAndUpdate(req.params.userId, req.body, {new: true})
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+
+//   // Delete a user and associated apps
+  deleteUser(req, res) {
+    Users.findByIdAndDelete(req.params.userId)
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : Thoughts.deleteMany({ _id: { $in: user.thoughts } })
+      )
+      .then(() => res.json({ message: 'User and associated apps deleted!' }))
+      .catch((err) => res.status(500).json(err));
+  },
+};
--- models/Users.js
+++ models/Users.js
@@ -1,5 +1,4 @@
 const { Schema, model } = require("mongoose");
-const userSchema = require("./Reactions");
 
 const userSchema = new Schema(
   {
