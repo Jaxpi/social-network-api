4814b067c0463e8b2f042e65d9cee8feae09b310
Jax I <jaxpi85@gmail.com>
2022-11-21 15:41:01 -0500

    creating models

---
+++ models/Thoughts.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reactions");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timeStamp) => new Date(timeStamp).toLocaleString(),
+    },
+    username: { type: String, required: true },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function() {
+    return this.reactions.length
+})
+
+module.exports = thoughtSchema;
