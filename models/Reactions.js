258a7a8a0aec1bdabb4925f3d8a7f2c86d2fec0e
Jax I <jaxpi85@gmail.com>
2022-11-22 21:09:00 -0500

    testing reaction schema in thoughts model

--- models/Reactions.js
+++ models/Reactions.js
@@ -1,25 +1,25 @@
-const { Schema, Types } = require("mongoose");
+// const { Schema, Types } = require("mongoose");
 
-const reactionSchema = new Schema(
-  {
-    reactionBody: {
-      type: String,
-      required: true,
-      maxLength: 280,
-    },
-    username: { type: String, required: true },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: (timeStamp) => new Date(timeStamp).toLocaleString(),
-    },
-  },
-  {
-    toJSON: {
-      getters: true,
-    },
-    id: false,
-  }
-);
+// const reactionSchema = new Schema(
+//   {
+//     reactionBody: {
+//       type: String,
+//       required: true,
+//       maxLength: 280,
+//     },
+//     username: { type: String, required: true },
+//     createdAt: {
+//       type: Date,
+//       default: Date.now,
+//       get: (timeStamp) => new Date(timeStamp).toLocaleString(),
+//     },
+//   },
+//   {
+//     toJSON: {
+//       getters: true,
+//     },
+//     id: false,
+//   }
+// );
 
-module.exports = reactionSchema;
+// module.exports = reactionSchema;
--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,5 +1,26 @@
 const { Schema, model } = require("mongoose");
-const reactionSchema = require("./Reactions");
+
+const reactionSchema = new Schema(
+  {
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    username: { type: String, required: true },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timeStamp) => new Date(timeStamp).toLocaleString(),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 const thoughtSchema = new Schema(
   {
